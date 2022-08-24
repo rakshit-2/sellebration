c0ab8a62bca559e73fc5ad1465d9bd0aa7294c75
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-08-24 12:18:53 +0530

    organism structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 const App=(props)=>{
+
+  
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" ></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
