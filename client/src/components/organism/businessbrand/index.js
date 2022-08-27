51ee6be8336ebcad43389d5b2658022af4a2013b
samarth2032 <samarth1174@chitkara.edu.in>
2022-08-27 20:49:53 +0530

    bussinessbrand struct

--- client/src/components/organism/businessbrand/index.js
+++ client/src/components/organism/businessbrand/index.js
@@ -1,12 +1,34 @@
 
 import './index.css';
 
-const BusiinessBrand=(props)=>{
+const BusinessBrand=(props)=>{
 return (
     <>     
-    BusiinessBrand
+    <div className="BusinessBrand__outer">
+        <div className="BusinessBrand__inner">
+            <div className="BusinessBrand__sec1">
+                <div className="BusinessBrand__sec1__head">
+                        Our Businesses
+                </div>
+                <div className="BusinessBrand__sec1__content">
+                Contrary to popular belief, Lorem Ipsum is not
+                 simply random text. It has roots in a piece of classical
+                  Latin literature from 45 BC, making it over 2000 years old
+                  . Richard McClintock, a Latin professor at Hampden-Sydney 
+                  College in Virginia, looked up one of the more obscure 
+                  Latin words, consectetur, from a Lorem Ipsum passage,
+                   and going through the cites of the word in classical
+                    literature, discovered the undoubtable source.
+                </div>
+                <div >
+
+                </div>
+            </div>
+
+        </div>
+    </div>
     </>
 );
 }
 
-export default BusiinessBrand;
+export default BusinessBrand;
