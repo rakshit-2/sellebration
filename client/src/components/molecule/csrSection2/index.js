25aec014a6cb709e780326aee1e6257b3cc044d3
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-08-29 00:19:40 +0530

    csr section complete last pdf section left non responsive

---
+++ client/src/components/molecule/csrSection2/index.js
@@ -0,0 +1,17 @@
+import './index.css';
+
+const CsrSection2=(props)=>{
+return (
+    <>
+    <div className='csr__inner__section2'>
+        <div className='csr__inner__section2__head'>
+            A ray of sunshine
+        </div>
+        <div className='csr__inner__section2__img'>
+        </div>
+    </div>
+    </>
+);
+}
+
+export default CsrSection2;
