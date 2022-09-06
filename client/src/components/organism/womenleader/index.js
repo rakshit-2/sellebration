7228500348250287ee37dbe23f775765f9159889
samarth2032 <samarth1174@chitkara.edu.in>
2022-09-06 15:02:16 +0530

    women leadership crousel

--- client/src/components/organism/womenleader/index.js
+++ client/src/components/organism/womenleader/index.js
@@ -7,48 +7,47 @@ import img from '../../assets/image/womenleadership_img/women_leadership_img.svg
 import WomenLeadershipCardData from './../../assets/store/womenleadershipcardData';
 import WomenLeadershipCard1 from '../../atom/womenleadershipCard1';
 const WomenLeadership=(props)=>{
-    const responsive = {
-        desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        },
-        tablet: {
-          breakpoint: { max: 1024, min: 750 },
-          items: 2,
-          slidesToSlide: 1 // optional, default to 1.
-        },
-        mobile: {
-          breakpoint: { max: 749, min: 0 },
-          items: 1,
-          slidesToSlide: 1 // optional, default to 1.
-        }
-      };
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 1 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 750 },
+      items: 2,
+      slidesToSlide: 1 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 749, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
 return (
     <>
     <div className="womenleader__outer">
         <div className="womenleader__inner">
             <Navbar  navDisplay={props.navDisplay} openNav={props.openNav}  closeNav={props.closeNav}/>
             <div className="womenleader__sec1">
-                <div className="womenleader__sec1__inner">
-                        <Carousel 
-                        responsive={responsive} 
-                        draggable
-                        autoPlay
-                        autoPlaySpeed={2000}
-                        pauseOnHover
-                        infinite
-                        showDots={true}
-                        removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
-                          {WomenLeadershipCardData.map((ele)=>{
-                    const{id,info,heading,name,title}=ele;
-                    return(
-                      <WomenLeadershipCard1 id={id} heading={heading} info={info} name={name} title={title}/>
-                    )
-                })}
-                            
-                        </Carousel>
-                    </div>
+                
+                    <Carousel 
+                    responsive={responsive} 
+                    draggable
+                    autoPlay
+                    autoPlaySpeed={2000}
+                    pauseOnHover
+                    infinite
+                    showDots={true}
+                    removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
+                    {WomenLeadershipCardData.map((ele)=>{
+                      const{id,info,heading,name,title}=ele;
+                      return(
+                        <WomenLeadershipCard1 id={id} heading={heading} info={info} name={name} title={title}/>
+                      )
+                    })}
+                    </Carousel>
+                
             </div>
         </div>
     </div>
