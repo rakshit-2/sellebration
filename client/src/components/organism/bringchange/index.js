f3b501c43f0901c94931ae5eb867b92caf58c542
samarth2032 <samarth1174@chitkara.edu.in>
2022-09-07 09:11:37 +0530

    bring change initial structure

--- client/src/components/organism/bringchange/index.js
+++ client/src/components/organism/bringchange/index.js
@@ -1,10 +1,22 @@
 
 import './index.css';
 
+import Navbar from './../NavBar/index';
+import button_image from ''
+
 const BringChange=(props)=>{
 return (
     <>
-    BringChange
+    <div className="bringchange__outer">
+        <div className="bringchange__inner">
+            <Navbar  navDisplay={props.navDisplay} openNav={props.openNav}  closeNav={props.closeNav}/>
+            <div className="bringchange__sec1">
+                <div>
+                    <img src={button_image}/>
+                </div>
+            </div>
+        </div>
+    </div>
     </>
 );
 }
