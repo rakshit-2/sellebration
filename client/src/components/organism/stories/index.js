4f80871ba5467800aa8c41d4e605cd96346db267
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-09-12 22:08:00 +0530

    filter complete stories full working

--- client/src/components/organism/stories/index.js
+++ client/src/components/organism/stories/index.js
@@ -3,23 +3,26 @@ import Navbar from './../NavBar/index';
 import { useEffect,useState } from 'react';
 import Axios from 'axios';
 import ApiLink from '../../assets/store/apiLink';
-import calendar_img from './../../assets/image/event/calendar_img.svg';
 import LoadingScreen from '../../atom/loadingScreen';
 import StoriesCard1 from './../../atom/storiesCard1/index';
-import test from './../../assets/image/test.png'
-
+import test from './../../assets/image/test.png';
+import Footer from './../../molecule/footer/index';
+import Footer2 from './../../molecule/footer2/index';
+import alert from './../../assets/image/event/alert.svg';
 
 const Stories=(props)=>{
     const[all,setAll]=useState();
     const[displayerData,setDisplayerData]=useState();
     const[company,setCompany]=useState();
+    const[topic,setTopic]=useState();
     const[cname,setCname]=useState("");
+    const[tname,setTname]=useState("");
     const[errHiding,setErrHiding]=useState("none");
     const[splitting,setSplitting]=useState();
+    const[loadMore,setLodeMore]=useState("flex")
     
     
-    
-    const[len,setLen]=useState(14);
+    const[len,setLen]=useState(8);
 
 
 
@@ -37,7 +40,67 @@ const Stories=(props)=>{
   }, []);
 
 
-  function filterAllData(li)
+  function getCompanyName(li)
+  {
+    if(li.length!==0)
+    {
+        var company_name=[{id:0,name:"Company Name"}];
+        var checker=[]
+        var k=1;
+        
+        for(var i=0;i<li.length;i++)
+        {
+            var flag=0;
+            for(var j=0;j<checker.length;j++)
+            {
+                if(checker[j]==li[i].name)
+                {
+                    flag=1;
+                    break
+                }
+            }
+            if(flag===0)
+            {
+                company_name.push({id:k,name:li[i].name});
+                checker.push(li[i].name)
+                k+=1;
+            }
+        }
+        setCompany(company_name);
+    }
+  }
+
+  function getTopicName(li)
+  {
+    if(li.length!==0)
+    {
+        var topic_name=[{id:0,topic:"Topic Name"}];
+        var checker=[];
+        var k=1;
+        
+        for(var i=0;i<li.length;i++)
+        {
+            var flag=0;
+            for(var j=0;j<checker.length;j++)
+            {
+                if(checker[j]==li[i].topic)
+                {
+                    flag=1;
+                    break
+                }
+            }
+            if(flag===0)
+            {
+                topic_name.push({id:k,topic:li[i].topic});
+                checker.push(li[i].topic)
+                k+=1;
+            }
+        }
+        setTopic(topic_name);
+    }
+  }
+
+  function getSplitting(len)
   {
     var ele=parseInt(len/3);
     var per=parseInt(len%3);
@@ -69,6 +132,13 @@ const Stories=(props)=>{
         }
     }
     setSplitting(lis);
+  }
+
+  function filterAllData(len,li)
+  {
+    getCompanyName(li);
+    getTopicName(li);
+    getSplitting(len);
     setStoriesLoading(false);
   }
 
@@ -81,12 +151,60 @@ const Stories=(props)=>{
           name:"all",
       }
       }).then((res)=>{
-            filterAllData(res.data);
+            filterAllData(len,res.data);
+            res.data.reverse();
+            setAll(res.data);
             setDisplayerData(res.data);
       })
   }, []);
 
 
+
+
+  function changeLen()
+  {
+    filterAllData(displayerData.length,[]);
+    setLen(displayerData.length);
+    setLodeMore("none");
+  }
+
+
+  function hidingError()
+  {
+      setErrHiding("none")
+  }
+
+  function filterClicked()
+  {
+    if((cname==="" || cname==="Company Name") || (tname==="" || tname==="Topic Name"))
+    {
+        setErrHiding("flex")
+        setTimeout(hidingError, 3000);
+        return;
+    }
+    else
+    {
+        var li=[]
+        for(var i=0;i<all.length;i++)
+        {
+            if(all[i].name===cname && all[i].topic===tname)
+            {
+                li.push(all[i])
+            }
+        }
+        
+        if(li.length===0)
+        {
+            setDisplayerData([{id:0,name:"No Story",topic:"-",date:"-",info:"",link:"",img:""}]);
+        }
+        else
+        {
+            getSplitting(li.length);
+            setDisplayerData(li);
+        }
+
+    }
+  }
   return (
     <>
     <div className="stories__outer">
@@ -108,17 +226,6 @@ const Stories=(props)=>{
             
 
             <div className='stories__inner__section2'>
-                <div className='stories__inner__section2__left__panel'>
-                    <select className='stories__inner__section2__left__panel__field' >
-                        <option>Company1</option>
-                    </select>
-                    <select className='stories__inner__section2__left__panel__field' >
-                        <option>Topic</option>
-                    </select>
-                    <div className='stories__inner__section2__button__filter'>
-                        Filters
-                    </div>
-                </div>
                 {
                     storiesLoading ? (
                         <div className='loading__outer'>
@@ -126,47 +233,86 @@ const Stories=(props)=>{
                         </div>
                         
                     ):(
-                        <div className='stories__inner__section2__display'>
-                            <div className='stories__inner__section2__col'>
-                                {displayerData.slice(splitting[0],splitting[1]).map((ele)=>{
-                                    const{id,name,date,info,topic,link}=ele;
-                                    var year=date.slice(0,4);
-                                    var month=parseInt(date.slice(5, 7));
-                                    var day=date.slice(8,10);
-                                    var date_value=day+" "+monthLis[month-1]+" "+year;
+                        <>
+                        <div className='stories__inner__section2__left__panel'>
+                            <select className='stories__inner__section2__left__panel__field' onChange={(e)=>[setCname(e.target.value)]}>
+                                {company.map((ele)=>{
+                                    const{id,name}=ele;
                                     return(
-                                        <StoriesCard1 id={id} name={name} date={date_value} info={info} topic={topic} link={link} img={test}/> 
+                                        <option key={id} value={name}>{name}</option>
                                     )
                                 })}
-                            </div>
-                            <div className='stories__inner__section2__col'>
-                                {displayerData.slice(splitting[2],splitting[3]).map((ele)=>{
-                                    const{id,name,date,info,topic,link}=ele;
-                                    var year=date.slice(0,4);
-                                    var month=parseInt(date.slice(5, 7));
-                                    var day=date.slice(8,10);
-                                    var date_value=day+" "+monthLis[month-1]+" "+year;
+                            </select>
+                            <select className='stories__inner__section2__left__panel__field' onChange={(e)=>[setTname(e.target.value)]}>
+                                {topic.map((ele)=>{
+                                    const{id,topic}=ele;
                                     return(
-                                        <StoriesCard1 id={id} name={name} date={date_value} info={info} topic={topic} link={link} img={test}/>
+                                        <option key={id} value={topic}>{topic}</option>
                                     )
                                 })}
-                            </div>
-                            <div className='stories__inner__section2__col'>
-                                {displayerData.slice(splitting[4],splitting[5]).map((ele)=>{
-                                    const{id,name,date,info,topic,link}=ele;
-                                    var year=date.slice(0,4);
-                                    var month=parseInt(date.slice(5, 7));
-                                    var day=date.slice(8,10);
-                                    var date_value=day+" "+monthLis[month-1]+" "+year;
-                                    return(
-                                        <StoriesCard1 id={id} name={name} date={date_value} info={info} topic={topic} link={link} img={test}/>
-                                    )
-                                })}
+                            </select>
+                            <div className='stories__inner__section2__button__filter' onClick={()=>{filterClicked()}}>
+                                Filters
                             </div>
                         </div>
+                            <div className='event__wronddata__modale' style={{display:errHiding}}>
+                                <div className='event__wronddata__modale__text'>
+                                    <img src={alert} className="event__wronddata__modale__text__img"></img>{'\u00A0'}{'\u00A0'}Invalid Information Entered 
+                                </div>
+                            </div>
+                            <div className='stories__inner__section2__display'>
+                                <div className='stories__inner__section2__col'>
+                                    {displayerData.slice(splitting[0],splitting[1]).map((ele)=>{
+                                        const{id,name,date,info,topic,link}=ele;
+                                        var year=date.slice(0,4);
+                                        var month=parseInt(date.slice(5, 7));
+                                        var day=date.slice(8,10);
+                                        var date_value=day+" "+monthLis[month-1]+" "+year;
+                                        return(
+                                            <StoriesCard1 id={id} name={name} date={date_value} info={info} topic={topic} link={link} img={test}/> 
+                                        )
+                                    })}
+                                </div>
+                                <div className='stories__inner__section2__col'>
+                                    {displayerData.slice(splitting[2],splitting[3]).map((ele)=>{
+                                        const{id,name,date,info,topic,link}=ele;
+                                        var year=date.slice(0,4);
+                                        var month=parseInt(date.slice(5, 7));
+                                        var day=date.slice(8,10);
+                                        var date_value=day+" "+monthLis[month-1]+" "+year;
+                                        return(
+                                            <StoriesCard1 id={id} name={name} date={date_value} info={info} topic={topic} link={link} img={test}/>
+                                        )
+                                    })}
+                                </div>
+                                <div className='stories__inner__section2__col'>
+                                    {displayerData.slice(splitting[4],splitting[5]).map((ele)=>{
+                                        const{id,name,date,info,topic,link}=ele;
+                                        var year=date.slice(0,4);
+                                        var month=parseInt(date.slice(5, 7));
+                                        var day=date.slice(8,10);
+                                        var date_value=day+" "+monthLis[month-1]+" "+year;
+                                        return(
+                                            <StoriesCard1 id={id} name={name} date={date_value} info={info} topic={topic} link={link} img={test}/>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                            <div className='stories__inner__section2__loadmore' style={{display:loadMore}}>
+                                <div className='stories__inner__section2__loadmore__button'  onClick={()=>{changeLen()}}>
+                                    Load More
+                                </div>
+                            </div>
+                            
+                        </>
                     )
                 }
             </div>
+
+            <Footer2/>
+
+            <Footer/>
+
         </div>
     </div>
     </>
