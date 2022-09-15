ae5b85af3a3a82eb7ae2527dbdf0c6db2e5e0478
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-09-15 10:23:38 +0530

    contact us validation complete for marked fields captcha left and post request left

--- client/src/components/organism/contactus/index.js
+++ client/src/components/organism/contactus/index.js
@@ -5,23 +5,151 @@ import Footer from './../../molecule/footer/index';
 import ContactUsSection1 from '../../molecule/contactusSection1';
 import ContactUsData from '../../assets/store/contactusData';
 import ReCAPTCHA from "react-google-recaptcha";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import alert from './../../assets/image/event/alert.svg';
 
 const ContactUs=(props)=>{
 
 
-  // scroll to top
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
+    // scroll to top
 
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, []);
+
+    const ariaLabel = { 'aria-label': 'description' };
+    const[name,setName]=useState("");
+    const[email,setEmail]=useState("");
+    const[organisation,setOrganisation]=useState("");
+    const[contactNumber,setContactNumber]=useState("");
+    const[queryIsFor,setQueryIsFor]=useState("Select Company");
+    const[quaryType,setQuaryType]=useState("Select Query");
+    const[yourQuery,setYourQuery]=useState("");
+    const[errorMessage,setErrorMessage]=useState("fill all * marked fields")
+    const[errHiding,setErrHiding]=useState("none");
+
+
+
+  
 
 
     function onChange(value) {
         console.log("Captcha value:", value);
     }
-return (
+
+
+    function checker(e,name)
+    {
+        switch(name) {
+            case "Name":
+                setName(e);
+                break;
+            case "Organisation":
+                setOrganisation(e);
+                break;
+            case "Email":
+                setEmail(e);
+                break;
+            case "Contact Number":
+                setContactNumber(e);
+                break;
+            default:
+                console.log("must not run if printing check file contactus/index.js default case")
+          }
+    }
+
+
+    function checkerSelect1(e,name)
+    {
+        setQueryIsFor(name);
+    }
+
+    function checkerSelect2(e,name)
+    {
+        setQuaryType(name);
+    }
+
+
+
+
+    function hidingError()
+    {
+        setErrHiding("none")
+    }
+
+
+    function ContactUsSubmit()
+    {
+        // var li=[name,email,organisation,contactNumber,queryIsFor,quaryType,yourQuery];
+        if(name==="")
+        {
+            setErrHiding("flex");
+            setErrorMessage("Fill Name Field");
+            setTimeout(hidingError, 3000);
+            return;
+        }
+        else
+        {
+            let flagname=/^[A-Za-z]+$/;
+            if(flagname.test(name)===false)
+            {
+                setErrHiding("flex");
+                setErrorMessage("Fill Name Field With A-Z/a-Z");
+                setTimeout(hidingError, 3000);
+                return;
+            }
+        }
+
+        if(email==="")
+        {
+            setErrHiding("flex");
+            setErrorMessage("Fill Email Field");
+            setTimeout(hidingError, 3000);
+            return;
+        }
+        else
+        {
+            var emailCheck=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+            if(emailCheck.test(email)===false)
+            {
+                setErrHiding("flex");
+                setErrorMessage("Fill Email Field With @abc@example.com");
+                setTimeout(hidingError, 3000);
+                return;
+            }
+        }
+        
+        if(queryIsFor==="Select Company")
+        {
+            setErrHiding("flex");
+            setErrorMessage("Select Company Name");
+            setTimeout(hidingError, 3000);
+            return;
+        }
+        
+        
+        if(quaryType==="Select Query")
+        {
+            setErrHiding("flex");
+            setErrorMessage("Select Quary Type");
+            setTimeout(hidingError, 3000);
+            return;
+        }
+
+
+        if(yourQuery==="")
+        {
+            setErrHiding("flex");
+            setErrorMessage("Your Query field is Empty");
+            setTimeout(hidingError, 3000);
+            return;
+        }
+
+
+    }
+
+    return (
     <>
     <div className="contactus__outer">
         <div className="contactus__inner">
@@ -30,13 +158,14 @@ return (
             
             <ContactUsSection1/>
 
-
-
             <div className='contactus__inner__seaction2'>
                 <div className='contactus__inner__seaction2__heading'>
                     Write to us
                 </div>
                 <div className='contactus__inner__seaction2__dispaly'>
+                    
+                    
+                    
                     {ContactUsData.input.map((ele)=>{
                         const{id,name,type,star,placeholder,inputfield}=ele;
                         return(
@@ -44,15 +173,18 @@ return (
                                 <div className='contactus__inner__seaction2__dispaly__each__heading'>
                                     {name}<span style={{color:"red",display:`${star}`}}>*</span>
                                 </div>
-                                <input type={type}placeholder={placeholder} className='contactus__inner__seaction2__dispaly__each__input'/>
+                                <input type={type} placeholder={placeholder} className='contactus__inner__seaction2__dispaly__each__input' onChange={(e)=>{checker(e.target.value,name)}} />
                             </div>
                         )
                     })}
+
+
+
                     <div className='contactus__inner__seaction2__dispaly__each'>
                         <div className='contactus__inner__seaction2__dispaly__each__heading'>
                             Query is for<span style={{color:"red"}}>*</span>
                         </div>
-                        <select className='contactus__inner__seaction2__dispaly__each__input'>
+                        <select className='contactus__inner__seaction2__dispaly__each__input' onChange={(e)=>{checkerSelect1(e.target.value,name)}}>
                             {ContactUsData.selectCompany.map((ele)=>{
                                 const{id,name}=ele;
                                 return(
@@ -65,7 +197,7 @@ return (
                         <div className='contactus__inner__seaction2__dispaly__each__heading'>
                             Query type<span style={{color:"red"}}>*</span>
                         </div>
-                        <select className='contactus__inner__seaction2__dispaly__each__input'>
+                        <select className='contactus__inner__seaction2__dispaly__each__input'  onChange={(e)=>{checkerSelect2(e.target.value,name)}}>
                             {ContactUsData.selectQuery.map((ele)=>{
                                 const{id,name}=ele;
                                 return(
@@ -78,7 +210,7 @@ return (
                         <div className='contactus__inner__seaction2__dispaly__each__heading'>
                             Your query<span style={{color:"red"}}>*</span>
                         </div>
-                        <textarea type={ContactUsData.selectTextarea.type} placeholder={ContactUsData.selectTextarea.placeholder} style={{height:"4rem",paddingLeft:"5px"}} className='contactus__inner__seaction2__dispaly__each__input'/>
+                        <textarea type={ContactUsData.selectTextarea.type} placeholder={ContactUsData.selectTextarea.placeholder} style={{height:"4rem",paddingLeft:"5px"}} className='contactus__inner__seaction2__dispaly__each__input' onChange={(e)=>{setYourQuery(e.target.value)}}/>
                     </div>
                     <div className='contactus__inner__seaction2__dispaly__each'>
                         <div className='contactus__inner__seaction2__dispaly__each__heading'>
@@ -91,11 +223,17 @@ return (
                     </div>
                     
                 </div>
+                <div className='conatctus__error__display' style={{display:errHiding}}>
+                    <div className='conatctus__error__display__inner'>
+                        <img src={alert} style={{width:"30px",height:"30px"}}/>{'\u00A0'}{errorMessage}
+                    </div>
+                </div>
                 <div className='contactus__inner__seaction2__bottom__out'>
-                    <div className='contactus__inner__seaction2__button'>
+                    <div className='contactus__inner__seaction2__button' onClick={()=>{ContactUsSubmit()}}>
                         Submit
                     </div>
                 </div>
+                
             </div>
 
 
