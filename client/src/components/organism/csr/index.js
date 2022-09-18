6d647d70c48cc4bc1f8b9dda74cead578d7e7ed3
rakshit4013 <rakshit1063@chitkara.edu.in>
2022-09-18 16:42:23 +0530

    csr full responsive

--- client/src/components/organism/csr/index.js
+++ client/src/components/organism/csr/index.js
@@ -39,115 +39,128 @@ return (
             <CsrSection1/>
 
             <CsrSection2/>
-
-            <div style={{width:"100%",height:"fit-content",overflowX:"hidden"}}>
-            {CsrCardData.map((ele)=>{
-                const{id,heading,img,text,button}=ele;
-                if(id%2===0)
-                {
-                    return(
-                        <div className='csr__inner__section3' data-aos="fade-left">
-                            <div className='csr__inner__section3__each1'>
-                                <div className='csr__inner__section3__each1__inner'>
-                                    <img src={img} className="csr__section3__img"/>
-                                </div>
-                            </div>
-                            <div className='csr__inner__section3__each2'>
-                                <div className='csr__inner__section3__each2__inner'>
-                                    <div className='csr__inner__section3__each2__inner__up'>
-                                        <div className='csr__inner__section3__each2__inner__heading'>
-                                            {heading}
-                                        </div>
-                                        <div className='csr__inner__section3__each2__inner__text'>
-                                            {text}
-                                        </div>
-                                    </div>
-                                    <div className='csr__inner__section3__each2__inner__down'>
-                                        <div className='csr__button'>
-                                            {button}
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-                else
-                {
-                    return (
-                        <div className='csr__inner__section3'  data-aos="fade-right">
-                            <div className='csr__inner__section3__each1' style={{justifyContent:"flex-end"}}>
-                                <div className='csr__inner__section3__each1__inner'>
-                                    <img src={img} className="csr__section3__img"/>
-                                </div>
-                            </div>
-                            <div className='csr__inner__section3__each2' style={{justifyContent:"flex-start"}}>
-                                <div className='csr__inner__section3__each2__inner'>
-                                    <div className='csr__inner__section3__each2__inner__up'>
-                                        <div className='csr__inner__section3__each2__inner__heading'>
-                                            {heading}
-                                        </div>
-                                        <div className='csr__inner__section3__each2__inner__text'>
-                                            {text}
-                                        </div>
-                                    </div>
-                                    <div className='csr__inner__section3__each2__inner__down'>
-                                        <div className='csr__button'>
-                                            {button}
-                                        </div>
-                                    </div>
-                                    
-                                </div>
-                            </div>
-                        </div>
-                    )
-                }
-            })}
-            </div>
-            
-            
-
-
-            <CsrSection4/>
-            
-
-            <div className='csr__inner__section5'>
-                <div className='csr__inner__section5__heading'>
-                    Our Initiatives
-                </div>
-                <div className='csr__inner__section5__display'  data-aos="fade-up">
-                    {CsrInitiativeData.map((ele)=>{
-                        const{id,img,heading,text}=ele;
+            <div className='csr__inner__inner'>
+                <div style={{width:"100%",height:"fit-content",overflow:"hidden"}}>
+                {CsrCardData.map((ele)=>{
+                    const{id,heading,img,text,button}=ele;
+                    if(id%2===0)
+                    {
                         return(
-                            <CsrCard2 id={id} img={img} heading={heading} text={text}/>
+                            <div className='csr__inner__section3' data-aos="fade-left">
+                                <div className='csr__inner__section3__each1'>
+                                    <div className='csr__inner__section3__each1__inner'>
+                                        <img src={img} className="csr__section3__img"/>
+                                    </div>
+                                </div>
+                                <div className='csr__inner__section3__each2'>
+                                    <div className='csr__inner__section3__each2__inner'>
+                                        <div className='csr__inner__section3__each2__inner__up'>
+                                            <div className='csr__inner__section3__each2__inner__heading'>
+                                                {heading}
+                                            </div>
+                                            <div className='csr__inner__section3__each2__inner__text'>
+                                                {text}
+                                            </div>
+                                        </div>
+                                        <div className='csr__inner__section3__each2__inner__down'>
+                                            <div className='csr__button'>
+                                                {button}
+                                            </div>
+                                        </div>
+                                        <div className='csr__inner__section3__each2__inner__down2'>
+                                            <div className='csr__button'>
+                                                More..
+                                            </div>
+                                        </div>
+                                        
+                                    </div>
+                                </div>
+                            </div>
                         )
-                    })}
+                    }
+                    else
+                    {
+                        return (
+                            <div className='csr__inner__section3'  data-aos="fade-right">
+                                <div className='csr__inner__section3__each1' style={{justifyContent:"flex-end"}}>
+                                    <div className='csr__inner__section3__each1__inner'>
+                                        <img src={img} className="csr__section3__img"/>
+                                    </div>
+                                </div>
+                                <div className='csr__inner__section3__each2' style={{justifyContent:"flex-start"}}>
+                                    <div className='csr__inner__section3__each2__inner'>
+                                        <div className='csr__inner__section3__each2__inner__up'>
+                                            <div className='csr__inner__section3__each2__inner__heading'>
+                                                {heading}
+                                            </div>
+                                            <div className='csr__inner__section3__each2__inner__text'>
+                                                {text}
+                                            </div>
+                                        </div>
+                                        <div className='csr__inner__section3__each2__inner__down'>
+                                            <div className='csr__button'>
+                                                {button}
+                                            </div>
+                                        </div>
+                                        <div className='csr__inner__section3__each2__inner__down2'>
+                                            <div className='csr__button'>
+                                                More..
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    }
+                })}
                 </div>
-            </div>
 
+
+            </div>
+            <div className='csr__inner__inner__special'>
+                <CsrSection4/>
+            </div>
+            
+            <div className='csr__inner__inner'>
+
+                <div className='csr__inner__section5'>
+                    <div className='csr__inner__section5__heading'>
+                        Our Initiatives
+                    </div>
+                    <div className='csr__inner__section5__display'  data-aos="fade-up">
+                        {CsrInitiativeData.map((ele)=>{
+                            const{id,img,heading,text}=ele;
+                            return(
+                                <CsrCard2 id={id} img={img} heading={heading} text={text}/>
+                            )
+                        })}
+                    </div>
+                </div>
+
+                
+            </div>
             <div className='csr__inner__section6'>
-                <div className='csr__inner__section6__inner'>
-                    <div className='csr__inner__section6__inner__each'  data-aos="fade-left">
-                        <div className='csr__inner__section6__inner__each__left'>
-                            Businesses:
+                    <div className='csr__inner__section6__inner'>
+                        <div className='csr__inner__section6__inner__each'  data-aos="fade-left">
+                            <div className='csr__inner__section6__inner__each__left'>
+                                Businesses:
+                            </div>
+                            <div className='csr__inner__section6__inner__each__left' style={{cursor:"pointer"}}>
+                                CSR Policy <img src={csr_pdf_img} className="csr__pdf__img"/>
+                            </div>
                         </div>
-                        <div className='csr__inner__section6__inner__each__left' style={{cursor:"pointer"}}>
-                            CSR Policy <img src={csr_pdf_img} className="csr__pdf__img"/>
+
+                        <div className='csr__inner__section6__inner__each'  data-aos="fade-right">
+                            <div className='csr__inner__section6__inner__each__left'>
+                                Sellebration:
+                            </div>
+                            <div className='csr__inner__section6__inner__each__left'  style={{cursor:"pointer"}}>
+                                Transcending business <img src={csr_pdf_img} className="csr__pdf__img"/>
+                            </div>
                         </div>
                     </div>
 
-                    <div className='csr__inner__section6__inner__each'  data-aos="fade-right">
-                        <div className='csr__inner__section6__inner__each__left'>
-                            Sellebration:
-                        </div>
-                        <div className='csr__inner__section6__inner__each__left'  style={{cursor:"pointer"}}>
-                            Transcending business <img src={csr_pdf_img} className="csr__pdf__img"/>
-                        </div>
-                    </div>
                 </div>
-
-            </div>
             <Footer/>
         </div>
     </div>
