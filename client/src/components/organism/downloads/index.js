82e2429287733701477477df36bb7a2dfc026558
rakshit-2 <rakhsit4013@gmail.com>
2022-10-02 08:23:26 +0530

    downloads responsive complete

--- client/src/components/organism/downloads/index.js
+++ client/src/components/organism/downloads/index.js
@@ -12,6 +12,11 @@ import ApiLink from '../../assets/store/apiLink';
 import LoadingScreen from '../../atom/loadingScreen';
 import Axios from 'axios';
 import img from "../../assets/image/test.png";
+import DownloadCard1 from '../../atom/downloadCard1';
+import DownloadCard2 from '../../atom/downloadCard2';
+
+
+
 
 const Downloads=(props)=>{
 
@@ -84,149 +89,149 @@ const Downloads=(props)=>{
         <div className='downloads__outer'>
             <div  className='downloads__inner'>
             <Navbar  navDisplay={props.navDisplay} openNav={props.openNav}  closeNav={props.closeNav}/>
-                <div className='downloads__sec1'>
-                    <div className='downloads__sec1__head'>
-                    Home / media
-                    </div>
-                    <div className='downloads__sec1__head__right'>
+                
+                <div className='downloads__inner__inner'>
+                        
+                    <div className='downloads__sec1'>
+                        <div className='downloads__sec1__head'>
+                            Home / media
+                        </div>
+                        <div className='downloads__sec1__head__right'>
                             <div className='downloads__sec1__head__right__inner'>
-                            Share
-                            <img style={{padding:"0 0 0 1rem"}} src={image}/>
+                                Share{'\u00A0'}{'\u00A0'}<img className='downloads__sec1__head__right__inner__icon' src={image}/>
                             </div>
-                    </div>
-                    <div className='downloads__sec1__bottom'>
-                        <div className='downloads__sec1__bottom__left' >
-                            <div className='downloads__sec1__bottom__left__1'>
-                            Downloads
-                            </div>
-                            <div className='downloads__sec1__bottom__left__2'>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-                            </div>
-                            <div className='downloads__sec1__bottom__left__1'>
-                            Lorem ipsum
-                            </div>
-                            <div className='downloads__sec1__bottom__left__2'>
-                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Loremtry's standard dummy text
-                            </div>
-                        </div >
-                        <div className='downloads__sec1__bottom__right' >
-                            <img className='downloads_head_logo' src={logo}/>
-                            <div className='downloads__sec1__bottom__right__inner'>
-                                <div className='downloads__sec1__bottom__right__inner__left'>
-                                    <div className='downloads__sec1__bottom__right__inner__each1'>
-                                    2D coloured logo <img src={down}/>
-                                    </div>
-                                    <div className='downloads__sec1__bottom__right__inner__each2'>
-                                    jpeg format (.zip 3.07 mb)
-                                    </div>
+                        </div>
+                        <div className='downloads__sec1__bottom'>
+                            <div className='downloads__sec1__bottom__left' >
+                                <div className='downloads__sec1__bottom__left__1'>
+                                    Downloads
                                 </div>
-                                <div className='downloads__sec1__bottom__right__inner__right'>
-                                <div className='downloads__sec1__bottom__right__inner__each1'>
-                                    3D coloured logo <img src={down}/>
+                                <div className='downloads__sec1__bottom__left__2'>
+                                    Lorem Ipsum is simply dummy text of the
+                                     printing and typesetting industry. Lorem 
+                                     Ipsum has been the industry's standard 
+                                     dummy text. Lorem Ipsum is simply dummy 
+                                     text of the printing and typesetting industry. 
+                                     Lorem Ipsum has been the industry's standard 
+                                     dummy text
+                                </div>
+                                <div className='downloads__sec1__bottom__left__1'>
+                                    Lorem ipsum
+                                </div>
+                                <div className='downloads__sec1__bottom__left__2'>
+                                    Lorem Ipsum is simply dummy text of the 
+                                    printing and typesetting industry. Lorem 
+                                    Ipsum has been the industry's standard dummy 
+                                    text. Loremtry's standard dummy text
+                                </div>
+                            </div >
+                            <div className='downloads__sec1__bottom__right' >
+                                <img className='downloads_head_logo' src={logo}/>
+                                <div className='downloads__sec1__bottom__right__inner'>
+                                    <div className='downloads__sec1__bottom__right__inner__left'>
+                                        <div className='downloads__sec1__bottom__right__inner__each1'>
+                                            2D coloured logo <img src={down}/>
+                                        </div>
+                                        <div className='downloads__sec1__bottom__right__inner__each2'>
+                                            jpeg format (.zip 3.07 mb)
+                                        </div>
                                     </div>
-                                    <div className='downloads__sec1__bottom__right__inner__each2'>
-                                    jpeg format (.zip 11.1 mb)
-
+                                    <div className='downloads__sec1__bottom__right__inner__left2'>
+                                        <div className='downloads__sec1__bottom__right__inner__each1'>
+                                            2D logo <img src={down}/>
+                                        </div>
+                                        <div className='downloads__sec1__bottom__right__inner__each2'>
+                                            (.zip 3.07 mb)
+                                        </div>
+                                    </div>
+                                    <div className='downloads__sec1__bottom__right__inner__left'>
+                                        <div className='downloads__sec1__bottom__right__inner__each1'>
+                                            3D coloured logo <img src={down}/>
+                                        </div>
+                                        <div className='downloads__sec1__bottom__right__inner__each2'>
+                                            jpeg format (.zip 11.1 mb)
+                                        </div>
+                                    </div>
+                                    <div className='downloads__sec1__bottom__right__inner__left2'>
+                                        <div className='downloads__sec1__bottom__right__inner__each1'>
+                                            3D logo <img src={down}/>
+                                        </div>
+                                        <div className='downloads__sec1__bottom__right__inner__each2'>
+                                            (.zip 11.1 mb)
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='downloads__sec2' data-aos="fade-up">
+                <div className='downloads__sec2'>
+                    <div className='downloads__inner__inner__crousal'>
                 {
-                                logLoading ? (
+                    logLoading ? (
 
-                                <div className='loading__outer'>
-                                    <LoadingScreen/>    
-                                </div>
-                                    
-                                ):(
-                                <Carousel
+                    <div className='loading__outer'>
+                        <LoadingScreen/>    
+                    </div>
+                        
+                    ):(
+                    <Carousel
 
-                                responsive={responsive} 
-                                draggable
-                                autoPlay
-                                autoPlaySpeed={2000}
-                                infinite
-                                showDots={true}
-                                removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
-                                {logData.map((ele)=>{
-                                const{id,size,name,zip}=ele;
-                                return(
-                                    <>
-                                    <div className='downloads__card1__outer'>
-                                        <div className='downloads__card1__img__outer'>
-                                            <img className='downloads__card1__img' src={img}/>  
-                                        </div>
-                                        <div className='downloads__card1__data'>
-                                            {name}
-                                        </div>
-                                        <div className='downloads__card1__button__outer'>
-                                            <div className='downloads__card1__button__inner'>
-                                            Download
-                                            </div>
-                                        </div>
-                                        <div className='downloads__card1__size'>
-                                            {size}
-                                        </div>
-
-                                    </div>
-                                    </>
-                                )
-                                })}
-                                </Carousel>
-                    )}
+                    responsive={responsive} 
+                    draggable
+                    autoPlay
+                    autoPlaySpeed={2000}
+                    infinite
+                    showDots={true}
+                    removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
+                    {logData.map((ele)=>{
+                    const{id,size,name,zip}=ele;
+                    return(
+                        <>
+                            <DownloadCard1 img={img} id={id} size={size} name={name}/>
+                        </>
+                    )
+                    })}
+                    </Carousel>
+                    )
+                }
+                </div>
                 </div>
                 <div className='downloads__sec3'>
                     <div className='downloads__sec3__head'>
-                    Company logos
+                        Company logos
                     </div>
                     <div className='downloads__sec3__card' data-aos="fade-up">
-
+                        <div className='downloads__inner__inner__crousal'>
                     {
-                                companyLoading ? (
+                        companyLoading ? (
 
-                                <div className='loading__outer'>
-                                    <LoadingScreen/>    
-                                </div>
-                                    
-                                ):(
-                            <Carousel 
-                                responsive={responsive} 
-                                draggable
-                                autoPlay
-                                autoPlaySpeed={2000}
-                                infinite
-                                showDots={true}
-                                removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
-                                {companyData.map((ele)=>{
-                                const{id,size}=ele;
-                                return(
-                                    <>
-                                    <div className='downloads__card2__outer'>
-                                        <div className='downloads__card2__img__outer'>
-                                            <img className='downloads__card2__img' src={img}/>  
-                                        </div>
-                                        
-                                        <div className='downloads__card2__button__outer'>
-                                            <div className='downloads__card2__button__inner'>
-                                            Download
-                                            </div>
-                                        </div>
-                                        <div className='downloads__card2__size'>
-                                            {size}
-                                        </div>
-
-                                    </div>
-                                    </>
-                                )
-                                })}
-                                </Carousel>
-                                )}
+                        <div className='loading__outer'>
+                            <LoadingScreen/>    
+                        </div>
+                            
+                        ):(
+                        <Carousel 
+                            responsive={responsive} 
+                            draggable
+                            autoPlay
+                            autoPlaySpeed={2000}
+                            infinite
+                            showDots={true}
+                            removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
+                            {companyData.map((ele)=>{
+                            const{id,size}=ele;
+                            return(
+                                <>
+                                    <DownloadCard2  img={img} id={id} size={size}/>
+                                </>
+                            )
+                            })}
+                        </Carousel>
+                        )
+                    }
+                        </div>
                     </div>
-
-
                 </div>
                 <Footer2/>
                 <Footer/>
