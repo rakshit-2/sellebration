be9757ae0617333a29b97d6ba75a3f5f819da709
samarth2032 <samarth1174@chitkara.edu.in>
2022-10-03 13:47:09 +0530

    logo structure

--- client/src/components/organism/logo/index.js
+++ client/src/components/organism/logo/index.js
@@ -2,7 +2,10 @@ import './index.css';
 import Navbar from './../NavBar/index';
 import image from '../../assets/image/innovation_img/innovation_img.svg';
 import { useEffect } from 'react';
-
+import Footer2 from './../../molecule/footer2/index';
+import Footer from './../../molecule/footer/index';
+import logo from '../../assets/image/navbar_img/navbar_logo_img.svg';
+import centerimg from '../../assets/image/logo/logo_mid.svg';
 const Logo=(props)=>{
       // scroll to top
 
@@ -34,12 +37,47 @@ return (
                     Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
                     </div>
                     <div className='logo__sec1__bottom__right'>
-                        
+                        <img class="logo__sec1__bottom__right__logo" src={logo}/>
                     </div>
 
                 </div>
             </div>
         </div>
+
+        <div className='logo__sec2'>
+            <div className='logo__sec2__head'>
+                Our new mark for new milestones
+            </div>
+
+            <div className='logo__sec2__mid'>
+                <div className='logo__sec2__mid__top'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum 
+                 is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                 has been the industry's standard dummy text ever Lorem Ipsum is simply 
+                 dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+                 the industry's standard dummy text ever Lorem Ipsum is simply dummy text 
+                 of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever 
+                </div>
+                <div className='logo__sec2__mid__center' >
+                    <img className='logo__sec2__mid__center_img' src={centerimg}/>
+                </div>
+                <div className='logo__sec2__mid__bottom'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever Lorem Ipsum 
+                 is simply dummy text of the printing and typesetting industry. Lorem Ipsum 
+                 has been the industry's standard dummy text ever Lorem Ipsum is simply 
+                 dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+                 the industry's standard dummy text ever Lorem Ipsum is simply dummy text 
+                 of the printing and typesetting industry.
+                 Lorem Ipsum has been the industry's standard dummy text ever 
+                </div>
+
+            </div>
+        </div>
+        <Footer2/>
+        <Footer/>
         </div>
     </div>
     </>
