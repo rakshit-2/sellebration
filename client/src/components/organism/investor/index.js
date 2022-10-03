92e4c160e337052882085ae820f794f6df32e5c5
rakshit-2 <rakhsit4013@gmail.com>
2022-10-03 21:13:43 +0530

    investors responsive complete

--- client/src/components/organism/investor/index.js
+++ client/src/components/organism/investor/index.js
@@ -66,14 +66,19 @@ const Investor=(props)=>{
         items: 4,
         slidesToSlide: 1
         },
-        tablet: {
-        breakpoint: { max: 1024, min: 750 },
+        smallLaptop: {
+        breakpoint: { max: 1120, min: 900 },
         items: 3,
+        slidesToSlide: 1
+        },
+        tablet: {
+        breakpoint: { max: 900, min: 750 },
+        items: 2,
         slidesToSlide: 1
         },
         mobile: {
         breakpoint: { max: 749, min: 0 },
-        items: 2,
+        items: 1,
         slidesToSlide: 1
         }
     };
@@ -89,130 +94,133 @@ return (
     <>
     <div className="inverstor__outer">
         <div className="investor__inner">
-            <Navbar  navDisplay={props.navDisplay} openNav={props.openNav}  closeNav={props.closeNav}/>
-            <div className="investor__inner__section1">
-                <div className="investor__inner__section1__inner">
-                    <div className="investor__inner__section1__inner__left">
-                        INVESTORS
-                    </div>
-                    <div className='investor__inner__section1__inner__right'>
-                        SHARE
-                    </div>
-                </div>
-            </div>
-            <div className="investor__inner__section2">
-                <div className="investor__inner__section2__heading">
-                    UPDATES
-                </div>
-                {
-                    updateLoading ? (
-                        <div className='loading__outer' >
-                            <LoadingScreen/>    
+                <Navbar  navDisplay={props.navDisplay} openNav={props.openNav}  closeNav={props.closeNav}/>
+                
+                <div className="investor__inner__section1">
+                    <div className="investor__inner__section1__inner">
+                        <div className="investor__inner__section1__inner__left">
+                            INVESTORS
                         </div>
-                        
-                    ):(
-                        <Carousel 
-                        responsive={responsive} 
-                        draggable
-                        autoPlay
-                        autoPlaySpeed={2000}
-                        pauseOnHover
-                        infinite
-                        showDots={true}
-                        removeArrowOnDeviceType={["tablet", "mobile","desktop"]}>
-                            {update.map((ele)=>{
-                                const{id,info}=ele;
-                                return(
-                                    <div key={id} className='investor__inner__section2__each'>
-                                        {info}
-                                    </div>
-                                )
-                            })}
-                        </Carousel>
-                    )
-                }
-            </div>
-            <div className='investor__inner__section3'>
-                <div className='investor__inner__section3__heading'>
-                    Company financials
-                </div>
-                {
-                    companyLoading ? (
-                        <div className='loading__outer'>
-                            <LoadingScreen/>    
+                        <div className='investor__inner__section1__inner__right'>
+                            SHARE
                         </div>
-                        
-                    ):(
-                    <div className='investor__inner__section3__displayouter'>
-                        {company.map((ele)=>{
-                            const{id,name,info}=ele;
+                    </div>
+                </div>
+                <div className='investor__inner__inner'>
+                <div className="investor__inner__section2">
+                    <div className="investor__inner__section2__heading">
+                        UPDATES
+                    </div>
+                    {
+                        updateLoading ? (
+                            <div className='loading__outer' >
+                                <LoadingScreen/>    
+                            </div>
                             
-                            if(id%2===0)
-                            {
-                                return(
-                                    <div key={id} className='investor__inner__section3__display'>
-                                        <div className='investor__inner__section3__display__left'>
-                                            <div className='investor__inner__section3__display__left__top'>
-                                                <img src={img} className="investor__inner__section3__display__left__top__pic"/>
-                                            </div>
-                                            <div className='investor__inner__section3__display__left__bottom'>
-                                                {name}
-                                            </div>
+                        ):(
+                            <Carousel 
+                            responsive={responsive} 
+                            draggable
+                            autoPlay
+                            autoPlaySpeed={2000}
+                            pauseOnHover
+                            infinite
+                            showDots={true}
+                            removeArrowOnDeviceType={["tablet","smallLaptop", "mobile","desktop"]}>
+                                {update.map((ele)=>{
+                                    const{id,info}=ele;
+                                    return(
+                                        <div key={id} className='investor__inner__section2__each'>
+                                            {info}
                                         </div>
-                                        <div className='investor__inner__section3__display__right'>
-                                            <div className='investor__inner__section3__display__right__top'>
-                                                {info}
-                                            </div>
-                                            <div className='investor__inner__section3__display__right__bottom'>
-                                                {innerEach.map((val)=>{
-                                                    const{id,name}=val;
-                                                    return(
-                                                        <div key={id} className='investor__inner__section3__display__right__bottom__each'>
-                                                            {name}
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                            else
-                            {
-                                return(
-                                    <div key={id} className='investor__inner__section3__display'>
-                                        <div className='investor__inner__section3__display__right'>
-                                            <div className='investor__inner__section3__display__right__top'>
-                                                {info}
-                                            </div>
-                                            <div className='investor__inner__section3__display__right__bottom'>
-                                                {innerEach.map((val)=>{
-                                                    const{id,name}=val;
-                                                    return(
-                                                        <div key={id} className='investor__inner__section3__display__right__bottom__each'>
-                                                            {name}
-                                                        </div>
-                                                    )
-                                                })}
-                                            </div>
-                                        </div>
-                                        <div className='investor__inner__section3__display__left'>
-                                            <div className='investor__inner__section3__display__left__top'>
-                                                <img src={img} className="investor__inner__section3__display__left__top__pic"/>
-                                            </div>
-                                            <div className='investor__inner__section3__display__left__bottom'>
-                                                {name}
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        })}
+                                    )
+                                })}
+                            </Carousel>
+                        )
+                    }
+                </div>
+                <div className='investor__inner__section3'>
+                    <div className='investor__inner__section3__heading'>
+                        Company financials
                     </div>
-                    )
-                }
-                
-                
+                    {
+                        companyLoading ? (
+                            <div className='loading__outer'>
+                                <LoadingScreen/>    
+                            </div>
+                            
+                        ):(
+                        <div className='investor__inner__section3__displayouter'>
+                            {company.map((ele)=>{
+                                const{id,name,info}=ele;
+                                
+                                if(id%2===0)
+                                {
+                                    return(
+                                        <div key={id} className='investor__inner__section3__display'>
+                                            <div className='investor__inner__section3__display__left'>
+                                                <div className='investor__inner__section3__display__left__top'>
+                                                    <img src={img} className="investor__inner__section3__display__left__top__pic"/>
+                                                </div>
+                                                <div className='investor__inner__section3__display__left__bottom'>
+                                                    {name}
+                                                </div>
+                                            </div>
+                                            <div className='investor__inner__section3__display__right'>
+                                                <div className='investor__inner__section3__display__right__top'>
+                                                    {info}
+                                                </div>
+                                                <div className='investor__inner__section3__display__right__bottom'>
+                                                    {innerEach.map((val)=>{
+                                                        const{id,name}=val;
+                                                        return(
+                                                            <div key={id} className='investor__inner__section3__display__right__bottom__each'>
+                                                                {name}
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                                else
+                                {
+                                    return(
+                                        <div key={id} className='investor__inner__section3__display' style={{flexWrap:"wrap-reverse"}}>
+                                            <div className='investor__inner__section3__display__right'>
+                                                <div className='investor__inner__section3__display__right__top'>
+                                                    {info}
+                                                </div>
+                                                <div className='investor__inner__section3__display__right__bottom'>
+                                                    {innerEach.map((val)=>{
+                                                        const{id,name}=val;
+                                                        return(
+                                                            <div key={id} className='investor__inner__section3__display__right__bottom__each'>
+                                                                {name}
+                                                            </div>
+                                                        )
+                                                    })}
+                                                </div>
+                                            </div>
+                                            <div className='investor__inner__section3__display__left'>
+                                                <div className='investor__inner__section3__display__left__top'>
+                                                    <img src={img} className="investor__inner__section3__display__left__top__pic"/>
+                                                </div>
+                                                <div className='investor__inner__section3__display__left__bottom'>
+                                                    {name}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    )
+                                }
+                            })}
+                        </div>
+                        )
+                    }
+                    
+                    
+                </div>
             </div>
             <Footer/>
         </div>
